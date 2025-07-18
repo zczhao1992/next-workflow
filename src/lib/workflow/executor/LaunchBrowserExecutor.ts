39f10e0e50dda473d04ca44523cad663f86d570b
Zhao.C <109458420+zczhao1992@users.noreply.github.com>
2025-07-18 03:38:12 +0000

    fix

--- src/lib/workflow/executor/LaunchBrowserExecutor.ts
+++ src/lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -29,6 +29,10 @@ export async function LaunchBrowserExecutor(
 
     const page = await browser.newPage();
 
+    page.setUserAgent(
+      "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"
+    );
+
     page.setViewport({ width: 2560, height: 1440 });
 
     await page.goto(websiteUrl);
