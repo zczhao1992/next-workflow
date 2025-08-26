49cbde1b0cdf0195dcca0c1aa501277117d73fb1
Zhao.C <109458420+zczhao1992@users.noreply.github.com>
2025-08-26 10:59:11 +0000

    fxi

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/workflows/(.*)*",
+  "/api/screenshot/(.*)*",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
